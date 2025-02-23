f07ac600ac18ad45a328be5817ed7cdbd6bd65b6
Jahid01234 <mdjahidhasan790523@gmail.com>
2025-02-24 00:29:23 +0600

    initial setup

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:assignment_1_homescreen/presentation/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:assignment_1_homescreen/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
